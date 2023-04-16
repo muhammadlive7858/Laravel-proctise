2671e537220b6f4ea361b4cda04c85479d032318
Your Name <you@example.com>
2023-04-16 09:10:38 +0300

    application create

---
+++ database/migrations/2023_04_16_060125_create_applications_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApplicationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('applications', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->foreignId('role_id')->canstrained()->onDelete('Cascade');
+            $table->string('title');
+            $table->string('message');
+            $table->string('file_url');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('applications');
+    }
+}
